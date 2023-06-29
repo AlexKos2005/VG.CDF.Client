b6430a687461538dab5fed2bbe98535a31b82578
Полеченко Алексей Александрович <49471330+AlexKos2005@users.noreply.github.com>
2023-06-29 16:57:35 +0300

    test

--- Application/WorkEmails/Queries/GetWorkEmailsListQuery.cs
+++ Application/WorkEmails/Queries/GetWorkEmailsListQuery.cs
@@ -4,6 +4,7 @@ namespace VG.CDF.Client.Application.WorkEmails.Commands;
 
 public class GetWorkEmailsListQuery
 {
+    //
     public Guid? Id { get; set; } = null;
     public Guid? ParameterReportTaskId { get; set; } = null;
     public string? Email { get; set; } = null;
