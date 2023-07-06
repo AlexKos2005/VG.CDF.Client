9a70c60e5cdf10de946c364e0cbafe1e1443e417
Полеченко Алексей Александрович <49471330+AlexKos2005@users.noreply.github.com>
2023-07-06 05:53:08 +0300

    feat: reports

--- Application/Dto/CheckedElementDto.cs
+++ Application/Dto/CheckedElementDto.cs
@@ -7,6 +7,11 @@ public class CheckedElementDto
     public Guid ElementId { get; set; }
     
     public int ElementExternalId { get; set; }
+
+    public string RuDescr { get; set; } = String.Empty;
     
+    public string UkrDescr { get; set; } = String.Empty;
+    
+    public string CurrentDescr { get; set; } = String.Empty;
     public bool IsChecked { get; set; }
 }
